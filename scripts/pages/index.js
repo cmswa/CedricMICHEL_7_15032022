1d53d6ff1644a036daea36b64e9206cbc0c1904b
Cédric Michel <c-e-d@live.fr>
2022-03-21 10:33:56 +0100

    factory recettes

---
+++ scripts/pages/index.js
@@ -0,0 +1,32 @@
+async function getRecipes() {
+    const jsonPath = './data/recipes.json';
+    const response =  await fetch(jsonPath);
+    const data = await response.json();
+    const recipes = data.recipes;
+    return recipes;
+}
+
+async function displayData(recipes) {
+    const recipesCard = document.querySelector(
+        '.card-body'
+    );
+
+    recipes.forEach((recipe) => {
+        recipesFactory(recipe)
+    });
+}
+
+async function init() {
+    // Récupère les datas des recettes
+    const  recipes  = await getRecipes();
+    // console.log(recipes);
+    recipes.forEach((recipe) => {
+        recipe.ingredients.forEach((ingredient) => {
+            // console.log(ingredient);
+        })
+        // // console.log(recipe);
+    })
+    displayData(recipes);
+}
+
+init();
