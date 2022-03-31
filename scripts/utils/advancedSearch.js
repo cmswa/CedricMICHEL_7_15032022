68fd58e3e5f6fa8eb163034204ebae3afd0a1d57
Cédric Michel <c-e-d@live.fr>
2022-03-31 14:30:08 +0200

    suite

--- scripts/utils/advancedSearch.js
+++ scripts/utils/advancedSearch.js
@@ -1,7 +1,10 @@
 const inputIngredientsDOM = document.getElementById('inputIngredients');
 const ingredientsAdvanced = document.querySelector('.ingredients');
+const appliancesAdvanced = document.querySelector('.appliances');
+const toolsAdvanced = document.querySelector('.tools');
 const advancedSearchDOM = document.querySelector('.advancedSearch');
 const chevron = document.querySelector('.chevronDown');
+const listIngredientsDOM = document.getElementById('listIngredients');
 
 function listIngredients(recipes) {
     recipes.forEach((recipe) => {
@@ -9,8 +12,8 @@ function listIngredients(recipes) {
         // // console.log(recipe);
     });
 
-    // au click input ingrédients passe à une largeur col-6 
-    ingredientsAdvanced.addEventListener('click', () => {
+    // au click input ingrédients passe à une largeur col-6
+    inputIngredientsDOM.addEventListener('click', () => {
         if (advancedSearchDOM.classList.contains('col-lg-5')) {
             advancedSearchDOM.classList.remove('col-lg-5');
         } else {
@@ -23,12 +26,32 @@ function listIngredients(recipes) {
             ingredientsAdvanced.classList.remove('col-6');
             ingredientsAdvanced.classList.add('col');
         }
+        if (appliancesAdvanced.classList.contains('col')) {
+            appliancesAdvanced.classList.remove('col');
+            appliancesAdvanced.classList.add('col-2');
+        } else {
+            appliancesAdvanced.classList.add('col');
+            appliancesAdvanced.classList.remove('col-2');
+        }
+        if (toolsAdvanced.classList.contains('col')) {
+            toolsAdvanced.classList.remove('col');
+            toolsAdvanced.classList.add('col-2');
+        } else {
+            toolsAdvanced.classList.add('col');
+            toolsAdvanced.classList.remove('col-2');
+        }
         if (chevron.classList.contains('chevronDown')) {
             chevron.classList.add('chevronDown-rotate');
             chevron.classList.remove('chevronDown');
         } else {
             chevron.classList.remove('chevronDown-rotate');
             chevron.classList.add('chevronDown');
+        }
+        //tags display none
+        if (listIngredientsDOM.classList.contains('off')) {
+            listIngredientsDOM.classList.remove('off');
+        } else {
+            listIngredientsDOM.classList.add('off');
         }
     });
 }
