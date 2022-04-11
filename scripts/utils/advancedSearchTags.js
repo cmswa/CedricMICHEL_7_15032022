00b1e859e92e49798917761fbf48cce8c04bc4c2
Cédric Michel <c-e-d@live.fr>
2022-04-11 15:23:18 +0200

    ajout de selectedTags pour appareils et ustensiles

--- scripts/utils/advancedSearchTags.js
+++ scripts/utils/advancedSearchTags.js
@@ -14,12 +14,13 @@ function createTagsUnderBar() {
     aside.className = 'selectedTags';
     // aside.className = 'selectedTags off';
     const ul = document.createElement('ul');
-    ul.className =
-        'd-flex align-content-around flex-wrap col-6 selectedTags__ul';
+    ul.className = 'd-flex align-content-around flex-wrap col-6 selectedTags__ul';
     aside.append(ul);
     searchBarContainer.append(aside);
 
     listenOnIngredientsInput();
+    listenOnAppliancesInput();
+    listenOnToolsInput();
 }
 
 // [{ nom: 'Pomme de terre', type: 'ingredient'}, { nom: 'spatule', type:'ustensile'}]
@@ -32,21 +33,72 @@ function listenOnIngredientsInput() {
             selectedTagsIngredient.push(item.textContent);
             const li = document.createElement('li');
             li.classList.add('selectedTagsIngredients');
+            li.classList.add('tag');
             const close = document.createElement('i');
             close.className = 'far fa-times-circle';
             close.content = '\f057';
             close.setAttribute('type', 'button');
             close.setAttribute('aria-label', 'Close');
-            //fermeture avec la croix d'un tag selectedTags 
+            // fermeture avec la croix d'un tag selectedTags
             close.setAttribute('onclick', "this.parentElement.style.display = 'none';");
             li.textContent = item.textContent;
             li.append(close);
-            const selectedTags__ulDOM =
-                document.querySelector('.selectedTags__ul');
+            const selectedTags__ulDOM = document.querySelector('.selectedTags__ul');
             selectedTags__ulDOM.append(li);
             console.log(selectedTagsIngredient);
             // createTagsUnderBar(recipes);
-            
+        });
+    });
+}
+
+const selectedTagsAppliance = [];
+function listenOnAppliancesInput() {
+    const appliancesLi = document.querySelectorAll('.listAppliances__li');
+    appliancesLi.forEach((item) => {
+        item.addEventListener('click', () => {
+            selectedTagsAppliance.push(item.textContent);
+            const li = document.createElement('li');
+            li.classList.add('selectedTagsAppliances');
+            li.classList.add('tag');
+            const close = document.createElement('i');
+            close.className = 'far fa-times-circle';
+            close.content = '\f057';
+            close.setAttribute('type', 'button');
+            close.setAttribute('aria-label', 'Close');
+            // fermeture avec la croix d'un tag selectedTags
+            close.setAttribute('onclick', "this.parentElement.style.display = 'none';");
+            li.textContent = item.textContent;
+            li.append(close);
+            const selectedTags__ulDOM = document.querySelector('.selectedTags__ul');
+            selectedTags__ulDOM.append(li);
+            console.log(selectedTagsAppliance);
+            // createTagsUnderBar(recipes);
+        });
+    });
+}
+
+const selectedTagsTool = [];
+function listenOnToolsInput() {
+    const toolsLi = document.querySelectorAll('.listTools__li');
+    toolsLi.forEach((item) => {
+        item.addEventListener('click', () => {
+            selectedTagsTool.push(item.textContent);
+            const li = document.createElement('li');
+            li.classList.add('selectedTagsTools');
+            li.classList.add('tag');
+            const close = document.createElement('i');
+            close.className = 'far fa-times-circle';
+            close.content = '\f057';
+            close.setAttribute('type', 'button');
+            close.setAttribute('aria-label', 'Close');
+            // fermeture avec la croix d'un tag selectedTags
+            close.setAttribute('onclick', "this.parentElement.style.display = 'none';");
+            li.textContent = item.textContent;
+            li.append(close);
+            const selectedTags__ulDOM = document.querySelector('.selectedTags__ul');
+            selectedTags__ulDOM.append(li);
+            console.log(selectedTagsTool);
+            // createTagsUnderBar(recipes);
         });
     });
 }
